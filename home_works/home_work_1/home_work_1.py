40c5c51514df7d5fcf0f14922223093963a98909
Alex760164 <titorenkosanya@gmail.com>
2020-12-04 18:15:39 +0200

    ДЗ-1. Оформление кода в соответствии с PEP8

--- home_works/home_work_1/home_work_1.py
+++ home_works/home_work_1/home_work_1.py
@@ -29,11 +29,19 @@ from http.cookies import SimpleCookie
 
 
 def parse_parameters(url):
+    """
+    Функция "parse_parameters" принимает строку запроса и извлекает (парсить) из нее переданные
+    параметры, результат возвращает в виде словаря.
+    """
     res = dict(parse.parse_qsl(parse.urlsplit(url).query))
     return res
 
 
 def parse_cookies(raw_data):
+    """
+    Функция "parse_cookies" принимает строку куков и извлекает (парсит) из нее куки, результат
+    возвращает в виде словаря.
+    """
     cookie = SimpleCookie()
     cookie.load(raw_data)
     dict_cookies = {}
@@ -47,24 +55,55 @@ if __name__ == '__main__':
     assert parse_parameters('http://example.com/?') == {}
     assert parse_parameters('http://example.com/') == {}
     assert parse_parameters('https://test_2.com/par=app') == {'par': 'app'}
-    assert parse_parameters('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
-    assert parse_parameters('https://example.com/page?name=Alex&age=21') == {'name': 'Alex', 'age': '21'}
-    assert parse_parameters('http://example.com/page/?name=Alexandr&age=22') == {'name': 'Alexandr', 'age': '22'}
-    assert parse_parameters('https://test_1.com/page?fruit=apple&color=red&price=14.00') == {'fruit': 'apple', 'color': 'red', 'price': '14.00'}
-    assert parse_parameters('http://e-catalog.ua/page?name=HP&model=Pavillion_dv5') == {'name': 'HP', 'model': 'Pavillion_dv5'}
-    assert parse_parameters('https://exam.com/page?name=Tests&page=21') == {'name': 'Tests', 'page': '21'}
-    assert parse_parameters('http://googlemap.com/page/?country=Ukraine&city=Odessa') == {'country': 'Ukraine', 'city': 'Odessa'}
+    assert parse_parameters('https://example.com/path/to/page?\
+                            name=ferret&color=purple') == {'name': 'ferret',
+                                                           'color': 'purple'}
+    assert parse_parameters('https://example.com/page?name=Alex\
+                            &age=21') == {'name': 'Alex',
+                                          'age': '21'}
+    assert parse_parameters('http://example.com/page/?name=Alexandr\
+                            &age=22') == {'name': 'Alexandr',
+                                          'age': '22'}
+    assert parse_parameters('https://test_1.com/page?fruit=apple&color=red\
+                            &price=14.00') == {'fruit': 'apple',
+                                               'color': 'red',
+                                               'price': '14.00'}
+    assert parse_parameters('http://e-catalog.ua/page?name=HP\
+                            &model=Pavillion_dv5') == {'name': 'HP',
+                                                       'model': 'Pavillion_dv5'}
+    assert parse_parameters('https://exam.com/page?name=Tests\
+                            &page=21') == {'name': 'Tests',
+                                           'page': '21'}
+    assert parse_parameters('http://googlemap.com/page/?country=Ukraine\
+                            &city=Odessa') == {'country': 'Ukraine',
+                                               'city': 'Odessa'}
     assert parse_parameters('https://test_2.com/page?par=app') == {'par': 'app'}
-    assert parse_parameters('http://e-catalog.ua/page?category=PC&model=DELL&color=black') == {'category': 'PC', 'model': 'DELL', 'color': 'black'}
+    assert parse_parameters('http://e-catalog.ua/page?category=PC&model=DELL\
+                            &color=black') == {'category': 'PC',
+                                               'model': 'DELL',
+                                               'color': 'black'}
 
     # Tests for function "parse_cookies"
     assert parse_cookies('') == {}
     assert parse_cookies('name=Dima;') == {'name': 'Dima'}
-    assert parse_cookies('devicePixelRatio=1;ident=exists;__utma=13103r6942.2918;__utmc=13103656942;') == {'devicePixelRatio': '1', 'ident': 'exists', '__utma': '13103r6942.2918', '__utmc': '13103656942'}
+    assert parse_cookies('devicePixelRatio=1;\
+                        ident=exists;\
+                        __utma=13103r6942.2918;\
+                        __utmc=13103656942;') == {'devicePixelRatio': '1',
+                                                  'ident': 'exists',
+                                                  '__utma': '13103r6942.2918',
+                                                  '__utmc': '13103656942'}
     assert parse_cookies('name=ferret;color=purple;') == {'name': 'ferret', 'color': 'purple'}
     assert parse_cookies('name=Alex;age=21;') == {'name': 'Alex', 'age': '21'}
-    assert parse_cookies('fruit=apple;color=red;price=14.00;') == {'fruit': 'apple', 'color': 'red', 'price': '14.00'}
-    assert parse_cookies('name=HP;model=Pavillion_dv5;') == {'name': 'HP', 'model': 'Pavillion_dv5'}
-    assert parse_cookies('name=Tests;page=21;par=app;') == {'name': 'Tests', 'page': '21', 'par': 'app'}
+    assert parse_cookies('fruit=apple;color=red;price=14.00;') == {'fruit': 'apple',
+                                                                   'color': 'red',
+                                                                   'price': '14.00'}
+    assert parse_cookies('name=HP;model=Pavillion_dv5;') == {'name': 'HP',
+                                                             'model': 'Pavillion_dv5'}
+    assert parse_cookies('name=Tests;page=21;par=app;') == {'name': 'Tests',
+                                                            'page': '21',
+                                                            'par': 'app'}
     assert parse_cookies('country=Ukraine;city=Odessa;') == {'country': 'Ukraine', 'city': 'Odessa'}
-    assert parse_cookies('category=PC;model=DELL;color=black;') == {'category': 'PC', 'model': 'DELL', 'color': 'black'}
+    assert parse_cookies('category=PC;model=DELL;color=black;') == {'category': 'PC',
+                                                                    'model': 'DELL',
+                                                                    'color': 'black'}
